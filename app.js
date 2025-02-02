98e45a625772cd3e5a12669e5f615266bc50215b
Afshan kanwal <kanwalafshan2244@gmail.com>
2025-02-03 01:30:15 +0500

    assignment1

---
+++ app.js
@@ -0,0 +1,141 @@
+// CHAPTER 1
+
+// Qno:1 Write a script to greet your website visitor using JS alert
+// box?
+// Ans:alert("hello!welcome to our website.");
+// Qno2:Write a script to display following message on your web
+// page?
+// Ans:alert("Error!please enter a valid password.");
+// Qno:3 Write a script to display following message on your web
+// page? (Hint : Use line break)
+// Ans:alert("welcome to JS Land\nHappy coding!");
+// Qno:4 Write a script to display following messages in sequence?
+// Ans: alert ("welcome to JS Land");
+        // alert("Happy coding")
+// Qno:5 Generate the following message through browser’s
+// developer console?
+// Ans: console.log("Hello...I can run through my web browser's console.");
+// Qno:6 Make use of alerts in your new/existing HTML & CSS
+// project?
+// Ans: alert("Hello!");
+// Qno:7 Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+// Ans:Body (inside your page’s HTML)
+
+// CHAPTER 2
+// Qno:1 Declare a variable called username.
+// Ans: let userName;
+// Qno:2 Declare a variable called myName & assign to it a string
+// that represents your Full Name?
+// Ans: let myname; "Afshan Kanwal"
+// Qno:3. Write script to?
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// Ans:(a)let jsvarialbe; "messege"
+//     (b)console.log ("hello world")
+//     (c)alert("Hello World")
+// Qno:4 Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes?
+// let userName = "Afshan kanwal"
+// let age = "23"
+// let course = "mobile web development"
+// console.log(userName, age, course);
+// alert (userName)
+// alert (age)
+// alert (course)
+// QNO:5 5. Write a script to display the following alert using one JS
+// variable?
+// Ans: alert ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+// Qno:6 Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box?(Hint: use
+// string concatenation).
+// let emailAddress = "kanwalafshan2244@gmail.com"
+// console.log(emailAddress);
+// alert (emailAddress)
+// Qno:7 Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box?
+// let bookName;
+// bookName = "A smarter way to learn JavaScript"
+// console.log(bookName);
+// alert (bookName)
+
+// Qno:8 Write a script to display this in browser through JS?
+//  Ans:document.write ("Yah!I can write HTML content through JavaScript");
+// Qno:9 Store following string in a variable and show in alert and
+// browser through JS?
+// Ans: alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+// CHAPTER  3
+// Qno:1 Declare a variable called age & assign to it your age. Show
+// your age in an alert box?
+// Ans: let age;
+// age = "I am 23 years old"
+// console.log (age)
+// alert (age)
+
+// Qno:2 Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”?
+//  Ans: let visitedWeb;
+// visitedWeb = "You have visited this site 14times"
+// alert(visitedWeb);
+
+// Qno:3 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browse?
+//  let birthYear;
+// birthYear =  1999
+// document.write ("My birth year is" + birthYear  )
+// document.write("</br> Data type of my ddeclared variable is number")
+
+// Qno:4 A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// Ans: let visitorName = "John Doe "
+// let projectTitle = "5 T-shirt(s) "
+// let quantityOder = "on XYZ Clothing store."
+// document.write (visitorName,projectTitle,quantityOder)
+
+
+// CHAPTER 4
+
+
+// Qno:1 Declare 3 variables in one statement?
+// Ans: let A, B, C ;
+
+// Qno:2 Declare 5 legal & 5 illegal variable names?
+// Ans: legal variable names 
+// let  afshanKanwal, _afshan, afshan123, $afshan;
+//illegal variable names
+// let #afshan, AfshaN-kanwal, 123afshan, afshan kawanl;
+
+// Qno:3 Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// 
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// Ans:document.write("<h1>Rules for naming JS variables</h1>")
+// document.write(" Variable names can only contain ($sign, my , _1st and variable).</br>")
+// document.write(" Variables must begin with a   ($name, _name or name).</br>")
+// document.write("Variable names are case  (case sensitive).</br>")
+// document.write("Variable names should not be JS (reserved words or keywords).")
